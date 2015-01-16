24488f422c67deb1817bcda28ba5babb927b8255
murphdizle <murphdizle@Hotmail.com>
2015-01-15 19:53:50 -0800

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -12,7 +12,18 @@ int main()
 	cout << "Bone: " << aBone->getTop() << endl;
 
 	Yard * deck = new Yard(1);
+	cout << "Print the deck: " << endl;
 	deck->print();	
+	
+	cout << endl << "After shuffle: " << endl;
+	deck->shuffleDeck();
+	deck->print();
+
+	cout << endl << "Deal a card: " << endl;
+	deck->deal(aBone);
+	aBone->print();
+	cout << endl;
+
 
 	return 0;
 }
