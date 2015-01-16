39a9e42d6fc7714801fb1e7de54975dec62d8c99
murphdizle <murphdizle@Hotmail.com>
2015-01-15 16:00:11 -0800

    Update Yard.cpp

--- Yard.cpp
+++ Yard.cpp
@@ -1,18 +1,32 @@
 #include "Yard.h"
+#include <iostream>
+using namespace std;
 
 
-Yard::Yard(int players, int bones):
-playerCount(players), boneCount(bones)
+Yard::Yard(int players):
+playerCount(players)
 {
+	players = 1;
+	Bone * currBone;
 	for (int i = 0; i <= 9; ++i){
 		for (int j = i; j <= 9; ++j){
-			Bone newBone(i, j);
-			Deck.push_back(&newBone);
+			currBone = new Bone(i, j);
+			deck.push_back(currBone);
 		}
 	}
 }
 
-
 Yard::~Yard()
 {
+}
+
+void Yard::print() const
+{
+	cout << endl;
+	for (auto it = deck.cbegin(); it != deck.cend(); it++)
+	{
+		(*it)->print();
+		cout<< endl << endl;
+	}
+	
 }
