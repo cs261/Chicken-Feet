a38f4021dfea7a3e4b500cbfdf8060a9b1a4b7f9
itsJonnyJyo <itsJonnyJyo@users.noreply.github.com>
2015-01-15 16:55:42 -0800

    Update hand.cpp

--- hand.cpp
+++ hand.cpp
@@ -1,29 +1,31 @@
-+#include<iostream>
-+#include"hand.h"
-+
-+Hand::Hand(){}
-+Hand:: ~Hand(){
-+	for(auto it = bones.begin(); it != bones.end(); it++){
-+		delete *it;
-+	}
-+}
-+
-+void Hand::addBone(Bone * bone){
-+	bones.push_back(bone);
-+}
-+
-+bool Hand::removeBone(int pos, Bone*& abone){
-+	if(pos < || pos >= bone.size())
-+		return false;
-+	abone = bones.at(pos);
-+	bones.erase(bones.begin + pos);
-+	return true;
-+
-+}
-+Void print() const {
-+	for(auto it = bones.cbegin(); it != bones.cend(); it++){
-+		(*it)->print();
-+		cout << " ";
-+	}
+#include<iostream>
+#include"hand.h"
+
+Hand::Hand(){}
+Hand:: ~Hand(){
+	for(auto it = hand.begin(); it != hand.end(); it++){
+		delete *it;
+	}
+}
+
+void Hand::addBone(Bone * bone){
+	hand.push_back(bone);
+}
+
+bool Hand::removeBone(int pos, Bone*& abone){
+	if(pos < || pos >= hand.size())
+		return false;
+	abone = hand.at(pos);
+	hand.erase(hand.begin + pos);
+	return true;
+
+}
+Void print() const {
+	for(auto it = hand.cbegin(); it != hand.cend(); it++){
+		(*it)->print();
+		cout << " ";
+	}
+	cout << endl << endl;
+}
 +	cout << endl << endl;
 +}
