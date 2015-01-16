155de60cc360b25233c0bf7458b7b17e48a1eac1
itsJonnyJyo <itsJonnyJyo@users.noreply.github.com>
2015-01-16 00:14:11 -0800

    Update Bone.cpp

--- Bone.cpp
+++ Bone.cpp
@@ -1,6 +1,8 @@
 #include "Bone.h"
 #include <iostream>
+
 using namespace std;
+
 
 Bone::Bone()
 {
@@ -9,16 +11,55 @@ Bone::Bone()
 Bone::Bone(int top, int bottom) :
 top(top), bottom(bottom)
 {
-
 }
-
 
 Bone::~Bone()
 {
 }
-void Bone::print() const 
+
+int Bone::getTop() const{
+	return top;
+}
+
+int Bone::getBottom() const{
+	return bottom;
+}
+
+bool Bone::getIsDouble() const{
+	return isDouble;
+}
+
+bool Bone::getTopUsed() const{
+	return topUsed;
+}
+
+bool Bone::getBottomUsed() const{
+	return bottomUsed;
+}
+
+void Bone::setTop(int newTop){
+	top = newTop;
+}
+
+void Bone::setBottom(int newBottom){
+	bottom = newBottom;
+}
+
+void Bone::setIsDouble(bool newBool){
+	isDouble = newBool;
+}
+
+void Bone::setTopUsed(bool newBool){
+	topUsed = newBool;
+}
+
+void Bone::setBottomUsed(bool newBool){
+	bottomUsed = newBool;
+}
+
+
+void Bone::print() const
 {
 	cout << endl;
 	cout << top << " " << bottom << endl;
 }
-
