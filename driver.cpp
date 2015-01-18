06b1c24baee59f36ad0c53b74f7731b5a0f25897
Scott <scott.e.pickthorn@gmail.com>
2015-01-17 16:59:03 -0800

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -23,8 +23,37 @@ int main()
 	deck->deal(aBone);
 	aBone->print();
 	cout << endl;
-
-
+	
+	Hand * aHand = new Hand();
+	aHand->addBone(new Bone(8,2));
+	aHand->addBone(new Bone(3,5));
+	aHand->addBone(new Bone(2,4));
+	aHand->addBone(new Bone(2,6));
+	aHand->addBone(new Bone(4,4));
+	
+	cout << "Hand after dealing:" << endl;
+	aHand->print();
+	cout << endl;
+	
+	aHand->removeBone(0, aBone);
+	aHand->removeBone(1, aBone);
+	
+	cout << "Hand after removing";
+	aHand->print();
+	
+	cout << "Player1 draws 5 bones: " << endl;
+	Player * player1 = new Player();
+	for(int i = 0; i < 5; i++){
+		player1->drawBone(yard);
+	}
+	player1->print();
+	
+	cout << "Player1 plays a bone: ";
+	player1->playBone(aBone);
+	aBone->print();
+	
+	cout << "Player1's Hand after playing: " << endl;
+	
 	return 0;
 }
 
